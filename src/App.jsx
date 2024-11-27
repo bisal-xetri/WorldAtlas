6b88d3a2a977e22077d021c6229e8038c8e3d270
bisal-xetri <bishaldkl56@gmail.com>
2024-11-27 21:57:32 +0545

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Country } from "./pages/Country";
+import { AppLayout } from "./components/layout/AppLayout";
+import { ErrorPage } from "./pages/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement:<ErrorPage/>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      { path: "country", element: <Country /> },
+      { path: "contact", element: <Contact /> },
+    ],
+  },
+]);
+const App = () => {
+  return <RouterProvider router={router}></RouterProvider>;
+};
+export default App;
---
+++ src/components/layout/UI/Footers.jsx
@@ -0,0 +1,7 @@
+
+
+export const Footers = () => {
+  return (
+   <h1>Hello footers</h1>
+  )
+}
